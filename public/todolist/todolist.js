7766c96e965e1ae20e5d266d6b2c3007b6a134c2
xShyQx <bartosz.kar@wp.pl>
2023-03-20 20:22:22 +0100

    Added rendering categories and tasks in todolist page

--- public/todolist/todolist.js
+++ public/todolist/todolist.js
@@ -1,4 +1,42 @@
+const returnBtn = document.getElementById("return-btn");
+const logoutBtn = document.getElementById("logout-btn");
+const categoryList = document.getElementById("category-list");
+const taskList = document.getElementById("task-list");
+
 fetch("/todolist/tasks")
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => {
+    let categoryString = categoryList.innerHTML;
+    data.categories.forEach(category => {
+        categoryString += `<div class="category" style="background-color: ${category.color}">
+        <input type="radio" name="category">
+        <label>${category.name}</label>
+        </div>`;
+    });
+    categoryList.innerHTML = categoryString;
+
+    if(data.tasks.length === 0)
+        taskList.innerHTML = "<p>Brak zadań</p>"
+    else {
+        let taskString = "";
+        data.tasks.forEach(task => {
+            taskString += `<div class="task">
+            <p>${task.content}</p>
+            </div>`;
+        });
+        taskList.innerHTML = taskString;
+    }
+})
 .catch(error => console.log(error));
+
+returnBtn.addEventListener("click", () => {
+    fetch("/home")
+    .then(response => window.location.href = response.url)
+    .catch(error => console.log(error));
+});
+
+logoutBtn.addEventListener("click", () => {
+    fetch("/logout")
+    .then(response => window.location.href = response.url)
+    .catch(error => console.log(error));
+});
